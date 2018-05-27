c736f80450bfa48455216a912a03df2ebf236fd8
Quenton Radford <qdradford@outlook.com>
2018-05-27 00:29:26 -0700

    base crawler finished

--- Services/scrape/service-web-scrape/src/main.py
+++ Services/scrape/service-web-scrape/src/main.py
@@ -1,20 +1,18 @@
 from models.scraperprofile import ScraperProfile
 from models.htmlobject import HtmlObject
 from scrapers.amazonscraper import AmazonScraper
-import requests
+import helpers.httpclient
 
     
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
 url = "https://www.amazon.com/dp/B01N259MDU/ref=nav_timeline_asin?_encoding=UTF8&psc=1";
-r = requests.get(url, headers=headers)
-data = r.text
-print(data)
+http = helpers.httpclient.HttpClient()
 
-
-htmlDoc = HtmlObject(data, None)
+htmlDoc = HtmlObject(http.get(url), None)
 profile = ScraperProfile("Amazon")
 webScraper = AmazonScraper(profile, htmlDoc)
 items = webScraper.pullProductInformation()
 print(items)
+
+
 
 
