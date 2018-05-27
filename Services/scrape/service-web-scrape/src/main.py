cd9e561687448e476b806058f8b3de11bf385ac3
Quenton Radford <qdradford@outlook.com>
2018-05-27 00:36:16 -0700

    fix imports

--- Services/scrape/service-web-scrape/src/main.py
+++ Services/scrape/service-web-scrape/src/main.py
@@ -1,11 +1,10 @@
 from models.scraperprofile import ScraperProfile
 from models.htmlobject import HtmlObject
 from scrapers.amazonscraper import AmazonScraper
-import helpers.httpclient
-
+from helpers.httpclient import HttpClient
     
 url = "https://www.amazon.com/dp/B01N259MDU/ref=nav_timeline_asin?_encoding=UTF8&psc=1"
-http = helpers.httpclient.HttpClient()
+http = HttpClient()
 
 htmlDoc = HtmlObject(http.get(url), None)
 profile = ScraperProfile("Amazon")
